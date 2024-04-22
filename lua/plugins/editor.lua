6bd3cdc07499ff780f4bf32e2ecdd2e610fd4467
Tony <97991338+tonypingco@users.noreply.github.com>
2024-04-22 10:20:07 +1000

    chore(feat): add surround feature for brackets

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -259,4 +259,14 @@ return {
     -- cmd = { "TodoQuickFix" },
     -- keys = {},
   },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
 }
